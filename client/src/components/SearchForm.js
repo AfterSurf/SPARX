a6660aab36aa7959c3356352dfcb163a2136ac11
Privat <privat@Laptopvlexander.fritz.box>
2023-09-19 21:52:58 +0200

    started adding mui and building controlled component

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,6 +1,15 @@
+import { useState } from "react";
+
 import  {useQueryNew}  from "../utils/gqlQueries"
 
+// mui: https://mui.com/material-ui/react-button/
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+
 export const SearchForm = () => {
+
+    const [name, setName] = useState("")
     
     const { data, loading, error, refetch } = useQueryNew({"name": "A"})
 
@@ -10,7 +19,9 @@ export const SearchForm = () => {
     return (
     <div>
         <h1>SEARCH FORM</h1>
-        <button onClick={() => {console.log("search ME! "); refetch({"name": "B"}); console.log(JSON.stringify(data))}}>SEARCH</button>
+        <TextField onChange={(e) => setName(e.target.value)}/>
+        <Button> MUI button</Button>
+        <button onClick={() => {console.log("search ME! ", name); refetch({"name": name}); console.log("data:",JSON.stringify(data))}}>SEARCH</button>
     </div>
     )
 }
